1d9704acd21a7c904cd580a04a7df52cfc517a54
blackninja81 <johnkaburu81@gmail.com>
2021-06-21 17:32:28 +0300

    remove race

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,9 +14,9 @@ function Card({ name, image, id, appearance, biography, work }) {
             <div className="stats">
                 <h5>Gender: {appearance.gender}</h5>
                 <h5>Race: {appearance.race}</h5>
-                <h5>Height: {appearance.height[0]}</h5>
+                {/* <h5>Height: {appearance.height[0]}</h5> */}
                 <h5>Aliases: {biography.aliases}</h5>
-                {/* <h5>Alignment: {biography.alignment}</h5> */}
+                <h5>Alignment: {biography.alignment}</h5>
                 <h5>Occupation: {work.occupation}</h5>
             </div>
         </div>
