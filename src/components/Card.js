e0b6097e8791d2d13df98c0db3c8c956fa775827
blackninja81 <johnkaburu81@gmail.com>
2021-06-21 11:55:37 +0300

    added search functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import "../App.js"
 
-function Card({name}) {
+function Card({name, image}) {
     return (
         <>
-        <div className="superCard">
-            <div className="card-image">
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.McoFxrPoHDAC1PDnm5g26wHaEK%26pid%3DApi&f=1" alt="superhero image" />
+            <div className="superCard">
+                <div className="front">
+                    <img src={image.url} alt="superhero image" />
+                    <div className="card-name">
+                        <h3>{name}</h3>
+                    </div>
+                </div>
             </div>
-            <div className="card-name">
-                <h3>{name}</h3>
-            </div>
-        </div>
         </>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,32 +3,76 @@ import Searchbox from "../layouts/Search";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
 
-const API = "https://superheroapi.com/api.php/875892883204575/500";
+const API = "https://superheroapi.com/api.php/875892883204575/search/";
 
 function Homepage() {
     const [hero, setHero] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
 
-    useEffect(() => {
-        fetch(API)
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(API + searchTerm)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
-                setHero(data);
+                console.log(data.results);
+                setHero(data.results);
             });
-    }, []);
+    };
+
+    const handleOnChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    // useEffect(() => {
+    //     fetch(API)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log(data.results);
+    //             setHero(data.results);
+    //         });
+    // }, []);
 
     return (
         <div className="mainPage">
             <h1>SuperHero Search</h1>
-            <Searchbox />
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            {hero.length > 0 && hero.map(
-                                (info) => <Card key={info.id} {...info} />)}
-                        </div>
+            <div class="container container-fluid">
+                <br />
+                <br />
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10 col-lg-8">
+                        <form class="card card-sm navigation" onSubmit={handleOnSubmit}>
+                            <div class="card-body row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <i class="fas fa-search h4 text-body"></i>
+                                </div>
+                                <div class="col">
+                                    <input
+                                        type="search"
+                                        value={searchTerm}
+                                        onChange={handleOnChange}
+                                        class="form-control form-control-lg form-control-borderless"
+                                        placeholder="Search Superhero"
+                                    />
+                                    <div class="col-auto">
+                                        <button class="btn btn-lg btn-outline-light" type="submit">Search</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
                     </div>
-                </div>   
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        {
+                            hero.map((info) => <Card {...info} />)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
