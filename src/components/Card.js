5540a59bb088a414b98bd2d84e8980abea037cf4
blackninja81 <johnkaburu81@gmail.com>
2021-06-21 17:31:16 +0300

    remove alignment

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,7 +16,7 @@ function Card({ name, image, id, appearance, biography, work }) {
                 <h5>Race: {appearance.race}</h5>
                 <h5>Height: {appearance.height[0]}</h5>
                 <h5>Aliases: {biography.aliases}</h5>
-                <h5>Alignment: {biography.alignment}</h5>
+                {/* <h5>Alignment: {biography.alignment}</h5> */}
                 <h5>Occupation: {work.occupation}</h5>
             </div>
         </div>
