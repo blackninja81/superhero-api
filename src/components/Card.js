b11276e649627df74373f2a405a9982bd36f5b56
blackninja81 <johnkaburu81@gmail.com>
2021-06-21 16:36:02 +0300

    font change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import "../App.js"
 import { Link } from "react-dom";
 
-function Card({ name, image, id, appearance, biography, work, connections }) {
+function Card({ name, image, id, appearance, biography, work }) {
     return (
         <div className="superInfo">
             <div className="superCard">
@@ -13,12 +13,12 @@ function Card({ name, image, id, appearance, biography, work, connections }) {
                 </div>
             </div>
             <div className="stats">
-                <h4>Gender: {appearance.gender}</h4>
-                <h4>Race: {appearance.race}</h4>
-                <h4>Height: {appearance.height[0]}</h4>
-                <h4>Aliases: {biography.aliases}</h4>
-                <h4>Alignment: {biography.alignment}</h4>
-                <h4>Occupation: {work.occupation}</h4>
+                <h5>Gender: {appearance.gender}</h5>
+                <h5>Race: {appearance.race}</h5>
+                <h5>Height: {appearance.height[0]}</h5>
+                <h5>Aliases: {biography.aliases}</h5>
+                <h5>Alignment: {biography.alignment}</h5>
+                <h5>Occupation: {work.occupation}</h5>
             </div>
         </div>
     )
